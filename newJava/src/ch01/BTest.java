71e8c8c89db0e5c15b3b402150ab7e7bd54a024f
kosmo <kosmo@DESKTOP-61K4Q3F>
2019-01-18 17:54:14 +0900

    2번째 커밋

--- newJava/src/ch01/BTest.java
+++ newJava/src/ch01/BTest.java
@@ -3,7 +3,8 @@ package ch01;
 public class BTest {
 	
 	public static void main(String[] args) {
-		System.out.println("BTEST ù��° Ŀ��");
+		System.out.println("BTEST ù��° Ŀ��");
+		System.out.println("BTEST 2번째 커밋");
 	}
 
 }
